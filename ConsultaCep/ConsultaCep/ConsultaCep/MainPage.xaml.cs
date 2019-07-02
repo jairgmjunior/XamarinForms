62a4402efe0e3c38f14db11b2f1c71bdbb9c4f7d
Jair Gomes de M. Junior <jair.gmjunior@hotmail.com>
2019-07-02 09:34:48 -0300

    consulta cep

---
+++ ConsultaCep/ConsultaCep/ConsultaCep/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using ConsultaCep.Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConsultaCep
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            txtCep.Focus();
+
+            btnBuscar.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            var cep = txtCep.Text.Trim();
+
+            try
+            {
+                if (IsValidCep(cep))
+                {
+                    var resultado = ViaCepService.BuscarEnderecoViaCep(cep);
+                    lbResultado.Text = $"Endereço: {resultado.Localidade}, {resultado.Uf}, {resultado.Logradouro}";
+                }
+                else
+                {
+                    DisplayAlert("Atenção", "Cép inválido!", "Ok");
+                    txtCep.Text = "";
+                    txtCep.Focus();
+                }
+            }
+            catch (Exception e)
+            {
+                DisplayAlert("Erro", "Não foi possível encontrar o cep", "Ok");
+            }
+
+        }
+
+        private bool IsValidCep(string cep)
+        {
+            //if(cep.Length != 8)
+            //{
+            //    return false;
+            //}
+            //else if (!cep.All(char.IsDigit))
+            //{
+            //    return false;
+            //}
+
+            return true;
+        }
+    }
+}
---
+++ ConsultaCep/ConsultaCep/ConsultaCep/Service/ViaCepService.cs
@@ -0,0 +1,26 @@
+﻿using ConsultaCep.Service.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ConsultaCep.Service
+{
+    public class ViaCepService
+    {
+        private static string _enderecoUrl = "http://viacep.com.br/ws/{0}/json/unicode/";
+
+        public static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            var novoEndereco = string.Format(_enderecoUrl, cep);
+
+            var wc = new WebClient();
+            var endereco = wc.DownloadString(novoEndereco);
+
+            var retorno = JsonConvert.DeserializeObject<Endereco>(endereco);
+
+            return retorno;
+        }
+    }
+}
