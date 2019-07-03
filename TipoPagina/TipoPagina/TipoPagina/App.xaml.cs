6d876b3016de00e27bc9181e1d7d6bc21b54cbc2
Jair Gomes de M. Junior <jair.gmjunior@hotmail.com>
2019-07-03 11:20:41 -0300

    Tipo de pagina

--- TipoPagina/TipoPagina/TipoPagina/App.xaml.cs
+++ TipoPagina/TipoPagina/TipoPagina/App.xaml.cs
@@ -10,7 +10,7 @@ namespace TipoPagina
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new Paginas.Carrocel.Introducao();
         }
 
         protected override void OnStart()
